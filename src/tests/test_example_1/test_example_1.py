81c0c15e7fcf5d3d9b0ff39ed57e7d6ee1509471
MarceliG <marcel9817@gmail.com>
2024-01-27 22:49:12 +0100

    prepared tox and test it

---
+++ src/tests/test_example_1/test_example_1.py
@@ -0,0 +1,10 @@
+class TestExample1:
+    """Test Example, in the future, should be removed."""
+
+    def test_addition(self):
+        """Test the addition of two numbers."""
+        number_one = 1
+        number_two = 2
+
+        result = number_one + number_two
+        assert result == 3
