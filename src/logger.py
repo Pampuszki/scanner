9a073fa1366ca777c2252dfaf7d62a79c09793f1
MarceliG <marcel9817@gmail.com>
2024-02-28 22:39:23 +0100

    changed formatter

--- src/logger.py
+++ src/logger.py
@@ -4,16 +4,13 @@ from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-formatter_stdout = logging.Formatter(
+formatter = logging.Formatter(
     "%(asctime)s | [%(filename)s:%(lineno)d] | %(levelname)s | %(message)s", "%d-%m-%Y %H:%M:%S"
-)
-formatter_file = logging.Formatter(
-    "%(asctime)s | [%(pathname)s:%(lineno)d] | %(levelname)s | %(message)s", "%d-%m-%Y %H:%M:%S"
 )
 
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(level=logging.DEBUG)
-stdout_handler.setFormatter(fmt=formatter_stdout)
+stdout_handler.setFormatter(fmt=formatter)
 
 file_handler = RotatingFileHandler(
     filename="logs.log",
@@ -23,7 +20,7 @@ file_handler = RotatingFileHandler(
     encoding="UTF-8",
 )
 file_handler.setLevel(level=logging.DEBUG)
-file_handler.setFormatter(fmt=formatter_file)
+file_handler.setFormatter(fmt=formatter)
 
 logger.addHandler(file_handler)
 logger.addHandler(stdout_handler)
